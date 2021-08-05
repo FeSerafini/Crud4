46eddf29263cccc9ee0d802c97bf02b57f12c195
Fernando Serafini <fecserafini@gmail.com>
2021-08-05 15:47:41 -0300

    Opções de produto agora são trazidas de uma array

--- angular/app.js
+++ angular/app.js
@@ -4,15 +4,82 @@ crud.controller("controller", function ($scope) {
 
    $scope.novoMaterial = {};
    $scope.materialSelecionado = {};
+   $scope.produtos = [{
+     idProduto: '1', 
+     produtoName: 'Rispalum®'},
+     {
+     idProduto: '2',
+      
+     produtoName: 'Rosulib'},
+     {
+     idProduto: '3',
+      
+     produtoName: 'Neotiapim'},
+     {
+     idProduto: '4',
+      
+     produtoName: 'Plagrel®'},
+     {
+     idProduto: '5',
+      
+     produtoName: 'Tegretol® / Tegretol® CR'},
+     {
+     idProduto: '6',
+      
+     produtoName: 'EXELON® PATCH'},
+     {
+     idProduto: '7',
+      
+     produtoName: 'RITALINA® LA'},
+     {
+     idProduto: '8',
+      
+     produtoName: 'RITALINA®'},
+     {
+     idProduto: '9',
+      
+     produtoName: 'STALEVO®'},
+     {
+     idProduto: '10',
+      
+     produtoName: 'TRILEPTAL®'},
+     {
+     idProduto: '11',
+      
+     produtoName: 'DIOVAN® HCT'},
+     {
+     idProduto: '12',
+      
+     produtoName: 'APRESOLINA®'},
+     {
+     idProduto: '13',
+      
+     produtoName: 'DIOVAN®'},
+     {
+     idProduto: '14',
+      
+     produtoName: 'COMTAN®'},
+     {
+     idProduto: '15',
+      
+     produtoName: 'Pidezot'},
+     {
+     idProduto: '16',
+      
+     produtoName: 'Lesdot'},
+     {
+     idProduto: '17',
+      
+     produtoName: 'Sandoz'}];
+
    $scope.materiais = [
     {
-        materialName: 'Mat1',
+        materialName: 'Experiencia e Evidencia Ansiedade',
         linha: '1',
-        tipo: '1',
-        produto: '6',
-        keyWord: 'ke1;key2;',
-        desc: 'Descrição 1',
-        arquivo: '',
+        tipo: '0',
+        produto: '16',
+        keyWord: 'ansiedade;neurologia;psiquiatria;',
+        desc: 'Eficacia e tolerabilidade de escilatopram',
         link: 'https://www.w3schools.com/angular/angular_intro.asp'
     },
     {
@@ -22,7 +89,6 @@ crud.controller("controller", function ($scope) {
         produto: '6',
         keyWord: 'ke1;key2;',
         desc: 'Descrição 1',
-        arquivo: '',
         link: 'https://www.w3schools.com/angular/angular_intro.asp'
     },
     {   
@@ -32,7 +98,6 @@ crud.controller("controller", function ($scope) {
         produto: '6',
         keyWord: 'ke1;key2;',
         desc: 'Descrição 1',
-        arquivo: '',
         link: 'https://www.w3schools.com/angular/angular_intro.asp'
     },
    ];
