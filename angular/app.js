e55b37416efac0bc0bac68138755845d199641bb
Fernando Serafini <fecserafini@gmail.com>
2021-08-10 17:33:14 -0300

    Botão fechar e x no canto do form agora resetam as innformações do modal, tipo, linha e produto agora são exibidos pelo nome e validação agora ocorre em todos os campos

--- angular/app.js
+++ angular/app.js
@@ -4,6 +4,20 @@ crud.controller("controller", function ($scope) {
 
    $scope.novoMaterial = {};
    $scope.materialSelecionado = {};
+   $scope.tipos = [
+       {idTipo : '0', tipoName :'Material Técnico'},
+       {idTipo : '1', tipoName :'Podcast'},
+       {idTipo : '2', tipoName :'Prestação de Serviço'},
+       {idTipo : '3', tipoName :'Webinar'},
+       {idTipo : '4', tipoName :'Sandoz Talks'}]
+      
+   $scope.linhas = [
+        {idLinha : '0', linhaName :'CVM'},
+        {idLinha : '1', linhaName :'SNC'},
+        {idLinha : '2', linhaName :'Hibrido'},
+        {idLinha : '3', linhaName :'Institucional'},
+        ]
+    
    $scope.produtos = [{
      idProduto: '1', 
      produtoName: 'Rispalum®'},
@@ -72,44 +86,44 @@ crud.controller("controller", function ($scope) {
       
      produtoName: 'Sandoz'}];
 
-   $scope.materiais = [
-    {
-        materialName: 'Experiencia e Evidencia Ansiedade',
-        linha: '1',
-        tipo: '0',
-        produto: '16',
-        keyWord: 'ansiedade;neurologia;psiquiatria;',
-        desc: 'Eficacia e tolerabilidade de escilatopram',
-        link: 'https://www.uol.com.br/vivabem/noticias/redacao/2018/07/17/ansiedade-o-que-e-quais-os-tipos-os-sintomas-e-tratamentos-mais-eficazes.htm',
-    },
-    {
-        materialName: 'Paciente em foco',
-        linha: '1',
-        tipo: '0',
-        produto: '1',
-        keyWord: 'paciente em foco;neurologia;',
-        desc: 'Estágio inicial da esquizofrenia',
-        link: 'https://saude.abril.com.br/mente-saudavel/o-que-e-esquizofrenia-sintomas-diagnostico-e-tratamento/',
-    },
-    {   
-        materialName: 'Score de Framingham',
-        linha: '0',
-        tipo: '2',
-        produto: '4',
-        keyWord: 'cardiologia;',
-        desc: 'Utilize para avaliar o risco de doença cardiovascular',
-        link: 'https://www.rededorsaoluiz.com.br/especialidades/cardiologia',
-    },
-    {   
-        materialName: 'Classificação Disturbius do Sono',
-        linha: '0',
-        tipo: '2',
-        produto: '15',
-        keyWord: 'insonia;cardiologia;neurologia;',
-        desc: 'Utilize para avaliar o risco de doença cardiovascular',
-        link: 'https://bvsms.saude.gov.br/disturbios-do-sono/',
-    },
-   ];
+     $scope.materiais = [
+        {
+            materialName: 'Experiencia e Evidencia Ansiedade',
+            linha: {idLinha : '1', linhaName:'SNC'},
+            tipo: {idTipo : '0', tipoName:'Material Técnico'},
+            produto: {idProduto: '16',produtoName: 'Lesdot'},
+            keyWord: 'ansiedade;neurologia;psiquiatria;',
+            desc: 'Eficacia e tolerabilidade de escilatopram',
+            link: 'https://www.uol.com.br/vivabem/noticias/redacao/2018/07/17/ansiedade-o-que-e-quais-os-tipos-os-sintomas-e-tratamentos-mais-eficazes.htm',
+        },
+        {
+            materialName: 'Paciente em foco',
+            linha: {idLinha : '1', linhaName:'SNC'},
+            tipo: {idTipo : '0', tipoName:'Material Técnico'},
+            produto: {idProduto: '1', produtoName: 'Rispalum®'},
+            keyWord: 'paciente em foco;neurologia;',
+            desc: 'Estágio inicial da esquizofrenia',
+            link: 'https://saude.abril.com.br/mente-saudavel/o-que-e-esquizofrenia-sintomas-diagnostico-e-tratamento/',
+        },
+        {   
+            materialName: 'Score de Framingham',
+            linha: {idLinha : '0', linhaName:'CVM'},
+            tipo: {idTipo : '2', tipoName:'Prestação de Serviço'},
+            produto: {idProduto: '4',produtoName: 'Plagrel®'},
+            keyWord: 'cardiologia;',
+            desc: 'Utilize para avaliar o risco de doença cardiovascular',
+            link: 'https://www.rededorsaoluiz.com.br/especialidades/cardiologia',
+        },
+        {   
+            materialName: 'Classificação Disturbius do Sono',
+            linha: {idLinha : '0', linhaName:'CVM'},
+            tipo: {idTipo : '2', tipoName:'Prestação de Serviço'},
+            produto: {idProduto: '15',produtoName: 'Pidezot'},
+            keyWord: 'insonia;cardiologia;neurologia;',
+            desc: 'Utilize para avaliar o risco de doença cardiovascular',
+            link: 'https://bvsms.saude.gov.br/disturbios-do-sono/',
+        },
+       ];
    
   
 
@@ -120,16 +134,16 @@ crud.controller("controller", function ($scope) {
    
   $scope.reset = function() {
     $scope.novoMaterial = {};
-    $scope.frm.$setPristine();
-    $scope.frm.$setUntouched();
-  }
+    $scope.frm1.$setPristine();
+    $scope.frm1.$setUntouched();
+  };
+  
 
    $scope.selecionaMaterial = function (material) {
         $scope.materialSelecionado = material;
    };
 
    $scope.alterarMaterial = function () {
-
    };
 
    $scope.excluirMaterial = function(){
